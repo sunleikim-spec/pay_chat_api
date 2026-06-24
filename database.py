25a60358f18ebdb5d4dcf11e41b5f674ea75aa54
lei <lei@leideMacBook-Pro.local>
2026-06-24 16:59:20 +0800

    1

--- database.py
+++ database.py
@@ -1,3 +1,4 @@
+from urllib.parse import quote_plus
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -13,9 +14,10 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env")
 
 settings = Settings()
+safe_pwd = quote_plus(settings.DB_PASSWORD)
 
 SQLALCHEMY_DATABASE_URL = (
-    f"mysql+pymysql://{settings.DB_USER}:{settings.DB_PASSWORD}"
+    f"mysql+pymysql://{settings.DB_USER}:{safe_pwd}"
     f"@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
 )
 
